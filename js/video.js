7f73fa5fa4c0b16ce3ffb91f064da4f5e3050a7b
Xiyu Hu <89854595+xhuscly@users.noreply.github.com>
2021-11-17 15:00:25 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,52 @@
+var video = document.getElementById("player1");
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate  * 0.95;
+	console.log(video.playbackRate);
+});
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log(video.playbackRate);
+});
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.duration - video.currentTime < 15){
+		video.currentTime = 0;
+		console.log(video.currentTime)
+	} else {
+		video.currentTime = video.currentTime + 15;
+		console.log(video.currentTime);
+	}	
+});
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted==false){
+		video.muted =true;
+		document.querySelector("#mute").innerHTML ='Unmute';
+	}else{
+		video.muted= false;
+		document.querySelector("#mute").innerHTML ='Mute';
+	}
+});
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume= this.value/100 ;
+	document.querySelector("#volume").innerHTML = video.volume*100 + "%";
+});
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
